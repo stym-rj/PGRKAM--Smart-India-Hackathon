efa1d8edd0fa4b24d0cd16e995a4db1584eb0ea6
satyam raj <satyam3279raj@gmail.com>
2023-09-27 09:27:16 +0530

    initial commit

---
+++ app/src/main/java/com/example/pgrkam/Authentication.kt
@@ -0,0 +1,11 @@
+package com.example.pgrkam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Authentication : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
