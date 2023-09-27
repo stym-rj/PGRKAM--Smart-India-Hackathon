5765d01406c363153f0131e1aef11605d5d04d58
satyam raj <satyam3279raj@gmail.com>
2023-09-27 10:56:26 +0530

    firebase initialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -44,4 +45,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // firebase bom
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+
+    // firebase analytics
+    implementation("com.google.firebase:firebase-analytics-ktx")
 }
